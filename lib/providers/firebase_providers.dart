95cc3a67d3acc7770d1cb9fef1b8b76ad39d04d9
Isheunesu Tembo <isheunesu48@gmail.com>
2025-01-17 10:18:37 +0300

    added categories ,searchtextfield to homescreen

--- lib/providers/firebase_providers.dart
+++ lib/providers/firebase_providers.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+
 final firebaseFireStoreProvider=Provider((ref)=>FirebaseFirestore.instance);
 final firebaseAuthProvider=Provider((ref)=>FirebaseAuth.instance);
 final firebaseStorageProvider=Provider((ref)=>FirebaseStorage.instance);
